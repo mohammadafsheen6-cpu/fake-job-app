9664fb0289c6e751984999ef3e5b7151d0e20e2b
mohammadafsheen6-cpu <mohammadafsheen6@gmail.com>
2026-04-01 19:33:34 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pickle
+
+st.title("Fake Job Detection")
+
+try:
+    model = pickle.load(open(r"C:\Users\user\Downloads\FakeJobProject\model.pkl", "rb"))
+    vectorizer = pickle.load(open(r"C:\Users\user\Downloads\FakeJobProject\vectorizer.pkl", "rb"))
+except:
+    st.error("Model files not loaded properly")
+    st.stop()
+
+user_input = st.text_area("Enter Job Description")
+
+if st.button("Predict"):
+    if user_input:
+        input_data = vectorizer.transform([user_input])
+        prediction = model.predict(input_data)
+
+        if prediction[0] == 1:
+            st.error("Fake Job")
+        else:
+            st.success("Real Job")
+    else:
+        st.warning("Enter text first")
